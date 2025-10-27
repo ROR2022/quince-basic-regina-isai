a7b827e596171a3e7d4d5911c40cd36bab7d0e6e
Ramiro Ocampo Rodriguez <101475348+ROR2022@users.noreply.github.com>
2025-10-27 12:42:15 -0600

    basic version1

--- app/page.jsx
+++ app/page.jsx
@@ -90,7 +90,7 @@ export default function WeddingInvitation() {
       <BasicCTA />
       
       {/* 🎵 Reproductor de audio fijo */}
-      {/*<AudioPlayer />*/}
+      <AudioPlayer />
     </div>
   )
 }
